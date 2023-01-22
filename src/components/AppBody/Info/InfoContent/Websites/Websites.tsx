ba8d2ebf82a7a04183fa51d3329cf9c3e6e0ffb5
eugenekuhot <kz.greenlinefamily@gmail.com>
2023-01-22 17:08:16 +0100

    set preloader after choose websites categories

--- src/components/AppBody/Info/InfoContent/Websites/Websites.tsx
+++ src/components/AppBody/Info/InfoContent/Websites/Websites.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import s from './Websites.module.scss'
 import WebsitesTopic from './WebsitesTopic';
 import {useLocation} from 'react-router-dom';
@@ -6,18 +6,25 @@ import {useDispatch} from 'react-redux';
 import {useAppSelector} from '../../../../../utils/Hooks/useAppSelector';
 import {getWebsites} from '../../../../../Redux/selectors/infoSelector';
 import {getWebsitesThunkCreator} from '../../../../../Redux/reducers/infoReducer';
+import Preloader from "../../../../utils/Preloader/Preloader";
 
 const Websites = () => {
 
   const dispatch = useDispatch()
   const websitesData = useAppSelector(getWebsites)
+  const [loadedStatus, setLoadedStatus] = useState<boolean>(false)
   let location = useLocation().pathname.split('/').pop() as string
   useEffect(() => {
+    setLoadedStatus(false)
     dispatch(getWebsitesThunkCreator(location))
   }, [location])
+  useEffect(() => {
+    setLoadedStatus(true)
+  }, [websitesData])
 
   return (
     <ul className={s.websitesList}>
+      {!loadedStatus && <Preloader/>}
       {websitesData.map((el: any) => <WebsitesTopic websitesData={el} key={el.id}/>)}
     </ul>
   )
