fafae5341bffcb5d2895756db056deb9488c0a8e
Kirill <apatski94@gmail.com>
2023-01-23 19:51:37 +0300

    refactoring expectations of the tests

--- src/Redux/selectors/tests/courseSelector.test.ts
+++ src/Redux/selectors/tests/courseSelector.test.ts
@@ -1,45 +1,20 @@
 import store, {AppStateType} from "../../redux-store";
 import {getCourseData} from "../courseSelector";
-import {courseType} from "../../reducers/courseReducer";
 
 let rootState: AppStateType;
-let startCourseData: Array<courseType>
 
 beforeEach(() => {
     rootState = store.getState()
-    startCourseData = [
-        {
-            itemTitle: 'Sprzęt komputerowy',
-            itemLink: '/course/instruments',
-            topics: [
-                {topicTitle: 'Test Title 1', topicLink: '/course/instruments/1'},
-                {topicTitle: 'Test Title 2', topicLink: '/course/instruments/2'},
-                {topicTitle: 'Test Title 3', topicLink: '/course/instruments/3'},
-                {topicTitle: 'Test Title 4', topicLink: '/course/instruments/4'},
-                {topicTitle: 'Test Title 5', topicLink: '/course/instruments/5'}
-            ]
-        },
-        {
-            itemTitle: 'Podstawy internetu',
-            itemLink: '/course/internet',
-            topics: [
-                {topicTitle: 'Test Title 1', topicLink: '/course/internet/1'},
-                {topicTitle: 'Test Title 2', topicLink: '/course/internet/2'},
-                {topicTitle: 'Test Title 3', topicLink: '/course/internet/3'},
-                {topicTitle: 'Test Title 4', topicLink: '/course/internet/4'},
-                {topicTitle: 'Test Title 5', topicLink: '/course/internet/5'}
-            ]
-        }
-    ]
 })
 
 
 it('get courses data', () => {
     const courseData = getCourseData(rootState);
+    const courseItemLink = courseData.map(course => course.itemLink)
+    const rootCourseItemLink = rootState.courses.courses.map(curse => curse.itemLink)
 
-    expect(courseData).toEqual(startCourseData);
-    expect(courseData.length).toBe(2);
-    expect(courseData).not.toBe(undefined);
-    expect(courseData.map(el => el.itemLink)[0]).toBe('/course/instruments');
-    expect(courseData.map(el => el.itemLink)[1]).toBe('/course/internet');
+    expect(courseData).toEqual(rootState.courses.courses);
+    expect(courseData.length).toBe(rootState.courses.courses.length);
+    expect(courseData).toBeDefined();
+    expect(courseItemLink).toEqual(rootCourseItemLink);
 })
