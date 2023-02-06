2606db4b611b172e0de57302ea6c1c4b7eab7d5c
MSokol <mihasokol1993@gmail.com>
2023-02-06 18:56:38 +0300

    created EmailNotification.tsx

--- src/components/AppBody/AuthPages/ConfirmRegistration/ConfirmRegistrationForm.tsx
+++ src/components/AppBody/AuthPages/ConfirmRegistration/ConfirmRegistrationForm.tsx
@@ -5,6 +5,7 @@ import '../../../../style.scss'
 import 'react-toastify/dist/ReactToastify.css'
 import {Nullable} from '../../../../Redux/redux-store'
 import PhoneForm from './PhoneForm/PhoneForm';
+import EmailNotification from './EmailNotification/EmailNotification';
 
 
 const confirmRegistrationForm: React.FC<PropsType> = ({onSubmit, themeStyle, fontSize}) => {
@@ -12,21 +13,7 @@ const confirmRegistrationForm: React.FC<PropsType> = ({onSubmit, themeStyle, fon
     <div className={cn(s.confirmRegistrationWrap, themeStyle ? themeStyle : '', s[themeStyle ? themeStyle : ''],
       s[fontSize ? fontSize : ''], [fontSize ? fontSize : ''])}>
       <PhoneForm onSubmit={onSubmit}/>
-      <div>
-        <label className={cn(s.confirmLabel, 'formLabel')}>
-          <span>Confirm your email address. Please check your mailbox and follow the instructions below. Email sent to: </span>
-          <p className={s.phone}>test@gmail.com</p>
-        </label>
-        <div className={s.buttonWrapper}>
-          <button className={cn(s.goBack, 'submitBtn')}>
-            Back
-          </button>
-          <button className={cn(s.continue, 'submitBtn')}>
-            Ok
-          </button>
-        </div>
-        <button className={s.repeatCode}>I didn't get a code</button>
-      </div>
+      <EmailNotification/>
     </div>
   )
 }
