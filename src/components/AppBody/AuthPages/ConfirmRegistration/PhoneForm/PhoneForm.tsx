b3a1b99cdd86bb277585f0e0af0b42a894668233
MSokol <mihasokol1993@gmail.com>
2023-02-06 18:53:03 +0300

    created PhoneForm.tsx

---
+++ src/components/AppBody/AuthPages/ConfirmRegistration/PhoneForm/PhoneForm.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import {Form, Formik} from 'formik';
+import cn from 'classnames';
+import s from '../ConfirmRegistration.module.scss';
+import {TextField} from '../../../../common/Form/FormControls/FormControls';
+import FormErrorMessage from '../../../../utils/FormErrorMessage/FormErrorMessage';
+import * as yup from 'yup';
+
+const confirmRegistrationFormSchema = yup.object().shape({
+  confirmationCode: yup.number().required('Code is required')
+})
+
+const PhoneForm: React.FC<PropsType> = ({onSubmit}) => {
+  return (
+    <Formik
+      initialValues={{confirmationCode: ''}}
+      validationSchema={confirmRegistrationFormSchema}
+      onSubmit={(values, {setSubmitting}) => {
+        setSubmitting(true)
+        onSubmit(values)
+        setSubmitting(false)
+      }}
+      validateOnChange={true}
+      validateOnBlur={true}
+    >
+      {({isSubmitting, touched, errors}) => (
+        <Form>
+          <label className={cn(s.confirmLabel, 'formLabel')}>
+            <span>Confirmation code was sent to:</span>
+            <p className={s.phone}>+ 380 (50) - 888 - 28 - **</p>
+            {TextField({
+              type: 'number',
+              name: 'confirmationCode',
+              placeholder: 'Enter the code from the message',
+              className: `textField ${errors.confirmationCode && touched.confirmationCode ? 'errorTextField' : ''}`
+            })}
+            {errors.confirmationCode && touched.confirmationCode &&
+                <FormErrorMessage>{errors.confirmationCode}</FormErrorMessage>}
+          </label>
+          <div className={s.buttonWrapper}>
+            <button type="submit" disabled={isSubmitting} className={cn(s.goBack, 'submitBtn')}>
+              Back
+            </button>
+            <button type="submit" disabled={isSubmitting} className={cn(s.continue, 'submitBtn')}>
+              Wyślij
+            </button>
+          </div>
+          <button className={s.repeatCode}>I didn't get a code</button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default PhoneForm;
+
+type PropsType = {
+  onSubmit: (formData: any) => void
+}
