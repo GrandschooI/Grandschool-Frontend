b2db5324b7d3a4895dcde4416c5279f08e259854
eugenekuhot <kz.greenlinefamily@gmail.com>
2023-02-23 00:02:02 +0100

    refactoring of App, index.tsx and scaffolding.ts

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,38 @@
-import React, {useEffect} from 'react'
-import {VerticleButton as ScrollUpButton} from 'react-scroll-up-button'
-import {Route, Switch} from 'react-router-dom';
-import {useDispatch} from 'react-redux';
-import {useAppSelector} from './utils/Hooks/useAppSelector';
-import {getLoadedInfo} from './Redux/selectors/styleSelector';
-import {setUserFromLocalStorage} from './Redux/reducers/userSlice';
+import React, { useEffect } from 'react';
+import { VerticleButton as ScrollUpButton } from 'react-scroll-up-button';
+import { Route, Switch } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useAppSelector } from './utils/Hooks/useAppSelector';
+import { getLoadedInfo } from './Redux/selectors/styleSelector';
+import { setUserFromLocalStorage } from './Redux/reducers/userSlice';
 import Preloader from './components/utils/Preloader/Preloader';
 import AppBody from './components/AppBody/AppBody';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 
-
 const App = () => {
-    const dispatch = useDispatch()
-    const isLoaded = useAppSelector(getLoadedInfo)
+    const dispatch = useDispatch();
+    const isLoaded = useAppSelector(getLoadedInfo);
 
     useEffect(() => {
-        dispatch(setUserFromLocalStorage())
-    }, [])
+        dispatch(setUserFromLocalStorage());
+    }, [dispatch]);
 
     return (
         <div className="App">
             <Switch>
-                <Route path="/" render={() => <section>
-                    <Header />
-                    <AppBody/>
-                    <Footer/>
-                    <ScrollUpButton/>
-                </section>}/>
+                <Route path="/">
+                    <section>
+                        <Header />
+                        <AppBody />
+                        <Footer />
+                        <ScrollUpButton />
+                    </section>
+                </Route>
             </Switch>
-            {!isLoaded && <Preloader/>}
+            {!isLoaded && <Preloader />}
         </div>
     );
-}
+};
 
-export default App
+export default App;
