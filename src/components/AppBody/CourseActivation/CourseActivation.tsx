c1c9d9533f948dab920a4d3d2286f3171f5cde9e
eugenekuhot <kz.greenlinefamily@gmail.com>
2023-02-22 23:43:43 +0100

    refactioring of CourseActivation.tsx

--- src/components/AppBody/CourseActivation/CourseActivation.tsx
+++ src/components/AppBody/CourseActivation/CourseActivation.tsx
@@ -1,64 +1,60 @@
-import React, {ChangeEvent, KeyboardEvent, useEffect, useRef, useState} from 'react';
-import Popup from "../../common/Popup/Popup";
-import cn from "classnames";
-import s from './CourseActivation.module.scss'
+import React, { ChangeEvent, KeyboardEvent, useEffect, useRef, useState } from 'react';
+import cn from 'classnames';
+import Popup from '../../common/Popup/Popup';
+import s from './CourseActivation.module.scss';
 
-
-let currentOtpIndex: number = 0;
 const CourseActivation = () => {
-  const [otp, setOtp] = useState<Array<string>>(new Array(5).fill(''))
-  const [activeOtpIndex, setActiveOtpIndex] = useState<number>(0)
+  const [otp, setOtp] = useState<string[]>(new Array(5).fill(''));
+  const [activeOtpIndex, setActiveOtpIndex] = useState<number>(0);
+  const inputRefs = useRef<HTMLInputElement[]>([]);
 
+  const onChangeHandle = (event: ChangeEvent<HTMLInputElement>, index: number): void => {
+    const { value } = event.currentTarget;
+    const newOtp = [...otp];
+    newOtp[index] = value.substring(value.length - 1);
+    setOtp(newOtp);
+    setActiveOtpIndex(value ? index + 1 : index - 1);
+  };
 
-  const inputRef = useRef<HTMLInputElement>(null)
-
-  const onChangeHandle = ({currentTarget}: ChangeEvent<HTMLInputElement>): void => {
-    const {value} = currentTarget
-    const newOTP = [...otp]
-    newOTP[currentOtpIndex] = value.substring(value.length - 1)
-    if(!value) setActiveOtpIndex(currentOtpIndex - 1)
-    else setActiveOtpIndex(currentOtpIndex + 1)
-    setOtp(newOTP)
-  }
-
-  const onKeyDownHandle = ({key}: KeyboardEvent<HTMLInputElement>, index: number) => {
-    currentOtpIndex = index
-    if(key === 'Backspace') {
-      setActiveOtpIndex(currentOtpIndex - 1)
+  const onKeyDownHandle = (event: KeyboardEvent<HTMLInputElement>, index: number) => {
+    if (event.key === 'Backspace' && index > 0) {
+      setActiveOtpIndex(index - 1);
+      inputRefs.current[index - 1].focus();
     }
-  }
+  };
 
   useEffect(() => {
-    inputRef.current?.focus()
-  }, [activeOtpIndex])
+    if (activeOtpIndex < otp.length) {
+      inputRefs.current[activeOtpIndex].focus();
+    }
+  }, [activeOtpIndex, otp.length]);
 
   return (
-    <div className={cn(s.courseActivationBody, 'container')}>
-      <h2>Активация курса</h2>
-      <Popup>
-        <p className={s.bodyDescription}>Что бы активировать курс, вам необходимо ниже ввести 5-значный пароль из e-mail
-          сообщения:</p>
-        <div className={s.bodyBoxCode}>
-          {
-            otp.map((_, index) => {
-              return (
+      <div className={cn(s.courseActivationBody, 'container')}>
+        <h2>Активация курса</h2>
+        <Popup>
+          <p className={s.bodyDescription}>
+            Чтобы активировать курс, вам необходимо ниже ввести 5-значный пароль из e-mail сообщения:
+          </p>
+          <div className={s.bodyBoxCode}>
+            {otp.map((value, index) => (
                 <input
-                  key={index}
-                  ref={activeOtpIndex === index ? inputRef : null}
-                  type="number"
-                  value={otp[index]}
-                  onChange={onChangeHandle}
-                  onKeyDown={(e) => onKeyDownHandle(e, index)}
+                    key={index}
+                    ref={(el) => (inputRefs.current[index] = el as HTMLInputElement)}
+                    type="number"
+                    value={value}
+                    onChange={(event) => onChangeHandle(event, index)}
+                    onKeyDown={(event) => onKeyDownHandle(event, index)}
                 />
-              )
-            })
-          }
-        </div>
-        <div style={{textAlign: "center"}}>
-          <button type="button" className={s.btn}>Активировать</button>
-        </div>
-      </Popup>
-    </div>
+            ))}
+          </div>
+          <div style={{ textAlign: 'center' }}>
+            <button type="button" className={s.btn}>
+              Активировать
+            </button>
+          </div>
+        </Popup>
+      </div>
   );
 };
 
