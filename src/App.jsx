2f582fb50b53f24c378c19879621b5be812bb6b7
EugeneKuhot <kz.greenlinefamily@gmail.com>
2023-02-24 13:01:33 +0100

    small beautification refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React, {useEffect} from 'react'
 import {VerticleButton as ScrollUpButton} from 'react-scroll-up-button'
-import {Route, Switch} from 'react-router-dom';
-import {useDispatch} from 'react-redux';
-import {useAppSelector} from './utils/Hooks/useAppSelector';
-import {getLoadedInfo} from './Redux/selectors/styleSelector';
-import {setUserFromLocalStorage} from './Redux/reducers/userSlice';
-import Preloader from './components/utils/Preloader/Preloader';
-import AppBody from './components/AppBody/AppBody';
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
+import {Route, Switch} from 'react-router-dom'
+import {useDispatch} from 'react-redux'
+import {useAppSelector} from './utils/Hooks/useAppSelector'
+import {getLoadedInfo} from './Redux/selectors/styleSelector'
+import {setUserFromLocalStorage} from './Redux/reducers/userSlice'
+import Preloader from './components/utils/Preloader/Preloader'
+import AppBody from './components/AppBody/AppBody'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
 
 
 const App = () => {
@@ -31,7 +31,7 @@ const App = () => {
             </Switch>
             {!isLoaded && <Preloader/>}
         </div>
-    );
+    )
 }
 
 export default App
