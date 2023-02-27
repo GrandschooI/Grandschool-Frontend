f906ec36621a6165d5e967221fbd67abdd9aef9d
eugenekuhot <kz.greenlinefamily@gmail.com>
2023-02-28 00:39:29 +0100

    refactoring of selectors

--- src/Redux/selectors/styleSelector.ts
+++ src/Redux/selectors/styleSelector.ts
@@ -1,26 +1,8 @@
 import {AppStateType, Nullable} from "../redux-toolkit-store";
 
-
-export const getStyleMode = (state: AppStateType): boolean => {
-  return state.styleMode.blindMode
-}
-
-export const getFontSize = (state: AppStateType): string => {
-  return state.styleMode.fontSize
-}
-
-export const getThemeStyle = (state: AppStateType): Nullable<string> => {
-  return state.styleMode.themeStyle
-}
-
-export const getImgAvailability = (state: AppStateType): boolean => {
-  return state.styleMode.images
-}
-
-export const getLoadedInfo = (state: AppStateType): boolean | undefined => {
-  return state.styleMode.isLoaded
-}
-
-export const getOptionsState = (state: AppStateType): boolean => {
-  return state.styleMode.isOptionsOpen
-}
+export const getStyleMode = (state: AppStateType): boolean => state.styleMode.blindMode
+export const getFontSize = (state: AppStateType): string => state.styleMode.fontSize
+export const getThemeStyle = (state: AppStateType): Nullable<string> => state.styleMode.themeStyle
+export const getImgAvailability = (state: AppStateType): boolean => state.styleMode.images
+export const getLoadedInfo = (state: AppStateType): boolean | undefined => state.styleMode.isLoaded
+export const getOptionsState = (state: AppStateType): boolean => state.styleMode.isOptionsOpen
