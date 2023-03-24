40a4cbe46b0c7651a941ac4b8d194a0af7e49e96
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-03-24 18:10:33 +0300

    build(husky): add pre-commit

--- src/components/AppBody/Payment/Payment.tsx
+++ src/components/AppBody/Payment/Payment.tsx
@@ -4,7 +4,6 @@ import cn from 'classnames'
 
 import s from './Payment.module.scss'
 
-console.log(1213)
 const Payment = () => {
   return (
     <section className={cn(s.paymentWrapper, 'container')}>
