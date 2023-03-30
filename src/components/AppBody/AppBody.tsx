6babcfd3bccfc13d4f35e2cad2b2f589efb14cae
Alexandr <alexkazakqq@gmail.com>
2023-03-30 12:10:33 +0300

    HTML template for search

--- src/components/AppBody/AppBody.tsx
+++ src/components/AppBody/AppBody.tsx
@@ -1,43 +1,47 @@
 import React from 'react'
-import {Redirect, Route, Switch} from 'react-router-dom'
+
+import cn from 'classnames'
+import { Redirect, Route, Switch } from 'react-router-dom'
+
+import { getOptionsState } from '../../Redux/selectors/styleSelector'
+import { useAppSelector } from '../../utils/Hooks/useAppSelector'
+
+import AboutUs from './AboutUs/AboutUs'
 import s from './AppBody.module.scss'
-import Teachers from './Teachers/Teachers'
-import cn from 'classnames';
+import AuthPage from './AuthPages/AuthPage'
+import ConfirmMail from './ConfirmMail/ConfirmMail'
+import Course from './Course/Course'
+import CourseActivation from './CourseActivation/CourseActivation'
+import HomePage from './HomePage/HomePage'
+import Info from './Info/Info'
+import NotFound from './NotFound/NotFound'
 import Payment from './Payment/Payment'
-import AboutUs from './AboutUs/AboutUs';
-import Course from './Course/Course';
-import HomePage from './HomePage/HomePage';
-import Info from './Info/Info';
-import NotFound from './NotFound/NotFound';
-import Profile from './Profile/Profile';
-import {useAppSelector} from '../../utils/Hooks/useAppSelector';
-import {getOptionsState} from "../../Redux/selectors/styleSelector";
-import AuthPage from './AuthPages/AuthPage';
-import ConfirmMail from "./ConfirmMail/ConfirmMail";
-import CourseActivation from "./CourseActivation/CourseActivation";
+import Profile from './Profile/Profile'
+import Teachers from './Teachers/Teachers'
 
 const AppBody = () => {
-    const isOptionsOpen = useAppSelector(getOptionsState)
-    return (
-        <div className={cn(s.appBody, isOptionsOpen ? s.openedBlindOption : '')}>
-            <Switch>
-                <Route exact path={"/"} render={() => <HomePage/>}/>
-                <Route path="/registration" render={() => <AuthPage/>}/>
-                <Route path="/confirm-registration" render={() => <AuthPage/>}/>
-                <Route path="/login" render={() => <AuthPage/>}/>
-                <Route path="/profile" render={() => <Profile/>}/>
-                <Route path="/about-us" render={() => <AboutUs/>}/>
-                <Route path="/course" render={() => <Course/>}/>
-                <Route path="/teachers" render={() => <Teachers/>}/>
-                <Route path="/info" render={() => <Info/>}/>
-                <Route path="/payment" render={() => <Payment/>}/>
-                <Route path="/email-verification" render={() => <ConfirmMail/>}/>
-                <Route path="/not-found" render={() => <NotFound/>}/>
-                <Route path="/course-activation" render={() => <CourseActivation/>}/>
-                <Redirect path="*" to={'/not-found'}/>
-            </Switch>
-        </div>
-    )
+  const isOptionsOpen = useAppSelector(getOptionsState)
+
+  return (
+    <div className={cn(s.appBody, isOptionsOpen ? s.openedBlindOption : '')}>
+      <Switch>
+        <Route exact path={'/'} render={() => <HomePage />} />
+        <Route path="/registration" render={() => <AuthPage />} />
+        <Route path="/confirm-registration" render={() => <AuthPage />} />
+        <Route path="/login" render={() => <AuthPage />} />
+        <Route path="/profile" render={() => <Profile />} />
+        <Route path="/about-us" render={() => <AboutUs />} />
+        <Route path="/course" render={() => <Course />} />
+        <Route path="/teachers" render={() => <Teachers />} />
+        <Route path="/info" render={() => <Info />} />
+        <Route path="/payment" render={() => <Payment />} />
+        <Route path="/email-verification" render={() => <ConfirmMail />} />
+        <Route path="/not-found" render={() => <NotFound />} />
+        <Route path="/course-activation" render={() => <CourseActivation />} />
+        <Redirect path="*" to={'/not-found'} />
+      </Switch>
+    </div>
+  )
 }
 
 export default AppBody
