9861ecc030bf863e5d656ec54d513534d63e13d6
e-nagornyak <dolkiv@gmail.com>
2023-04-02 00:23:38 +0300

    Added yellowTheme for CommentBlock

--- src/components/AppBody/Course/Comments/CommentItem/CommentItem.tsx
+++ src/components/AppBody/Course/Comments/CommentItem/CommentItem.tsx
@@ -1,11 +1,19 @@
 import React, { FC, memo } from 'react'
 
+import cn from 'classnames'
+
+import { getFontSize, getThemeStyle } from '../../../../../Redux/selectors/styleSelector'
+import { useAppSelector } from '../../../../../utils/Hooks/useAppSelector'
+
 import { CommentBlock } from './CommentBlock/CommentBlock'
 import s from './CommentItem.module.scss'
 
 export const CommentItem: FC = memo(() => {
+  const themeStyle = useAppSelector(getThemeStyle)
+  const fontSize = useAppSelector(getFontSize)
+
   return (
-    <div className={s.wrapper}>
+    <div className={cn(s.wrapper, s[themeStyle ? themeStyle : ''], 'commentItem', s[fontSize])}>
       <CommentBlock />
       <div className={s.answers}>
         <CommentBlock withImg={false} />
