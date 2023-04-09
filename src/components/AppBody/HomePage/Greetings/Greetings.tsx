13b02e61ff30db0b5757e52303ce5010b5c530f7
eugenekuhot <kz.greenlinefamily@gmail.com>
2023-04-09 17:11:11 +0200

    Fix styles for Greetings

--- src/components/AppBody/HomePage/Greetings/Greetings.tsx
+++ src/components/AppBody/HomePage/Greetings/Greetings.tsx
@@ -3,24 +3,16 @@ import React from 'react'
 import cn from 'classnames'
 import ReactPlayer from 'react-player/youtube'
 
-import {
-  getOptionsState,
-  getStyleMode,
-  getThemeStyle,
-} from '../../../../Redux/selectors/styleSelector'
+import { getThemeStyle } from '../../../../Redux/selectors/styleSelector'
 import { useAppSelector } from '../../../../utils/Hooks/useAppSelector'
 
 import s from './Greetings.module.scss'
 
 const Greetings = () => {
-  const blindMode = useAppSelector(getStyleMode)
   const themeStyle = useAppSelector(getThemeStyle)
-  const isOptionsOpen = useAppSelector(getOptionsState)
-
-  const isBlindActive = () => (blindMode && isOptionsOpen ? s.blindVideoBlock : '')
 
   return (
-    <section className={cn(s.videoBlock, s[themeStyle ? themeStyle : ''], isBlindActive())}>
+    <section className={cn(s.videoBlock, s[themeStyle ? themeStyle : ''])}>
       <ReactPlayer
         url="https://www.youtube.com/watch?v=S_zMbLa_nAE"
         controls={true}
