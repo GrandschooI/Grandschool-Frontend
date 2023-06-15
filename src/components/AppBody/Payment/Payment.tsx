dc7dd3bf13d9c2f64e0b5070d1c94f003afad119
eugenekuhot <kz.greenlinefamily@gmail.com>
2023-06-15 23:41:16 +0200

    payment success desktop

--- src/components/AppBody/Payment/Payment.tsx
+++ src/components/AppBody/Payment/Payment.tsx
@@ -2,53 +2,59 @@ import React from 'react'
 
 import cn from 'classnames'
 
-import Popup from '../../common/PopupSection/Popup/Popup'
-
 import s from './Payment.module.scss'
 import { PaymentSuccess } from './PaymentSuccess/PaymentSuccess'
 
 const Payment = () => {
   return (
-    <section className={cn(s.paymentWrapper, 'container')}>
-      <h1>Покупка курса</h1>
-      <div className={s.paymentOffer}>
-        <div>
-          <h3>БАЗОВЫЙ</h3>
-          250 PLN 320
-          <ul>
-            <li>
-              <h4>Что включает в себя курс</h4>
-              <span>20 занятий/38 часов с практическими занятиями</span>
-            </li>
-            <li>
-              <h4>Что включает в себя курс</h4>
-              <span>20 занятий/38 часов с практическими занятиями</span>
-            </li>
-            <li>
-              <h4>Что включает в себя курс</h4>
-              <span>20 занятий/38 часов с практическими занятиями</span>
-            </li>
-            <li>
-              <h4>Что включает в себя курс</h4>
-              <span>20 занятий/38 часов с практическими занятиями</span>
-            </li>
-            <li>
-              <h4>Что включает в себя курс</h4>
-              <span>20 занятий/38 часов с практическими занятиями</span>
-            </li>
-          </ul>
-          <button className={'inverseBtn'}>Назад</button>
-        </div>
-        <div>
-          <h3>Покупка курса “Базовый”</h3>
-          <p>
-            Здесь текст о курсе, например подробрности и уточнения. Например: курс будет доступен в
-            течении 1 года с момента покупки.{' '}
-          </p>
-          <p>
-            Для покупки курса вам необходимо ввести актуальный адрес электронной почты, на который
-            придет подтверждение вашей оплаты, чек и доступ к курсу.
-          </p>
+    <section className={s.paymentWrapper}>
+      <div className={'container'}>
+        <h1>Покупка курса</h1>
+        <div className={s.paymentOffer}>
+          <div className={s.descWrapper}>
+            <h3>БАЗОВЫЙ</h3>
+            <div className={s.priceWrappper}>
+              <span>250</span>
+              <sup>PLN</sup>
+              <span>320</span>
+            </div>
+            <ul>
+              <li>
+                <h4>Что включает в себя курс</h4>
+                <span>20 занятий/38 часов с практическими занятиями</span>
+              </li>
+              <li>
+                <h4>Что включает в себя курс</h4>
+                <span>20 занятий/38 часов с практическими занятиями</span>
+              </li>
+              <li>
+                <h4>Что включает в себя курс</h4>
+                <span>20 занятий/38 часов с практическими занятиями</span>
+              </li>
+              <li>
+                <h4>Что включает в себя курс</h4>
+                <span>20 занятий/38 часов с практическими занятиями</span>
+              </li>
+              <li>
+                <h4>Что включает в себя курс</h4>
+                <span>20 занятий/38 часов с практическими занятиями</span>
+              </li>
+            </ul>
+            <button className={'inverseBtn'}>Назад</button>
+          </div>
+          <div>
+            <h3>Покупка курса “Базовый”</h3>
+            <div className={s.bankDetailsWrapper}>
+              <p className={s.entryText}>
+                Здесь текст о курсе, например подробрности и уточнения. Например: курс будет
+                доступен в течении 1 года с момента покупки.
+              </p>
+              <p>
+                Для покупки курса вам необходимо ввести актуальный адрес электронной почты, на
+                который придет подтверждение вашей оплаты, чек и доступ к курсу.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
